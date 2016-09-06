1130dc0931d6b712d3c3ce8f7cb7cb8f6099b7fe
hareesha <hareesha1906@gmail.com>
2016-09-06 11:21:12 -0700

    Add implementaiton for equals and hashCode

--- Examples/src/main/java/com/Practice/Examples/Person.java
+++ Examples/src/main/java/com/Practice/Examples/Person.java
@@ -19,17 +19,35 @@ public class Person {
 	public void setId(String id) {
 		Id = id;
 	}
-/**
- * equals method need to be over ridden when we have to compare the user defined classes
- */
-	@Override
-	public boolean equals(Object o){
-		boolean result=false;
-			if(o!=null){
-				if(this.getId().equals(((Person)o).getId())){
-					return true;
-				}
-			}
-			return result;
-		}
+@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Person other = (Person) obj;
+	if (Id == null) {
+		if (other.Id != null)
+			return false;
+	} else if (!Id.equals(other.Id))
+		return false;
+	if (name == null) {
+		if (other.name != null)
+			return false;
+	} else if (!name.equals(other.name))
+		return false;
+	return true;
+}
+	
+	
 	}
