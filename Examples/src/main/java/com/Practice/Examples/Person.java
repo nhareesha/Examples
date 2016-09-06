fd75867b8d92c102f37161214386d9febbaada83
hareesha <hareesha1906@gmail.com>
2016-09-06 11:27:08 -0700

    update

--- Examples/src/main/java/com/Practice/Examples/Person.java
+++ Examples/src/main/java/com/Practice/Examples/Person.java
@@ -5,49 +5,44 @@ package com.Practice.Examples;
  *
  */
 public class Person {
-	String name;
-	String Id;
-	public String getName() {
-		return name;
+	private String name;
+	private String id;
+	
+	public Person(String name, String id){
+		this.id=id;
+		this.name=name;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getId() {
-		return Id;
-	}
-	public void setId(String id) {
-		Id = id;
-	}
-@Override
+
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
 		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Person other = (Person) obj;
-	if (Id == null) {
-		if (other.Id != null)
-			return false;
-	} else if (!Id.equals(other.Id))
-		return false;
-	if (name == null) {
-		if (other.name != null)
-			return false;
-	} else if (!name.equals(other.name))
-		return false;
-	return true;
-}
+	}
 	
 	
 	}
