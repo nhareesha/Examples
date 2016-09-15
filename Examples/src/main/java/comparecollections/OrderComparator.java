f19d719c94e09cb5fe6207e8510a4ce499a8632f
hareesha <hareesha1906@gmail.com>
2016-09-15 13:18:11 -0700

    Add OrderComparator class - implements Comparator interface

---
+++ Examples/src/main/java/comparecollections/OrderComparator.java
@@ -0,0 +1,14 @@
+package comparecollections;
+
+import java.util.Comparator;
+
+public class OrderComparator implements Comparator<Order> {
+
+	public int compare(Order o1, Order o2) {
+		if(o1!= null && o2 != null){
+			//int result= o1.getLocation() < o2.getLocation();
+		}
+		return 0;
+	}
+
+}
